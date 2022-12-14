f5c4a0b5d54efda7206f8b21fdac392e2afc9c75
Alex K <example@gmail.com>
2022-12-14 11:55:36 +0700

    next

--- src/components/pokemon-details/PokemonDetails.js
+++ src/components/pokemon-details/PokemonDetails.js
@@ -1,8 +1,17 @@
 import './pokemon-details.scss'
+import noImg from "../../pictures/no-image.webp"
 
 const PokemonDetails = ({pokemonSelected}) => {
+
+    
+    
     return (
-        <div>PokemonDetails</div>
+        <div className='pokermon-details-wrapper'>
+            <h3 className='pokermon-details-title'>Pokemon Title</h3>
+            <div className='pokermon-details-img'>
+            <img src={noImg} alt='pokemon'></img>
+            </div>
+        </div>
     )
 }
 
